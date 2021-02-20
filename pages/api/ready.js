6b2c055780dd3dc0706b4cc209271985a525db3f
yusitnikov <yu.sitnikov@synergetica.co>
2021-02-20 18:01:46 +0200

    Fix reading JSON body in /api/ready

--- pages/api/ready.js
+++ pages/api/ready.js
@@ -1,32 +1,46 @@
 // An HTTP webhook for the "Entry Status Equals" HTTP notification for the READY status (2) should point to /api/ready
 import {compileByEntryId} from "./compile/[entryId]";
 
-export default function ready(request, response) {
+// Don't try to parse the POST body automatically
+export const config = {
+    api: {
+        bodyParser: false,
+    },
+};
+
+export default async function ready(request, response) {
     console.log("    entry ready");
 
     if (request.method !== "POST") {
         console.error("    request is not POST")
         response.status(500).send("Internal server error");
+        return;
     }
 
-    let {data} = request;
+    const dataStr = await new Promise(resolve => {
+        let dataStr = "";
+        request.on("data", chunk => dataStr += chunk);
+        request.on("end", () => resolve(dataStr));
+    });
 
-    if (!data) {
+    if (!dataStr) {
         console.error("    empty POST data")
         response.status(500).send("Internal server error");
+        return;
     }
 
-    if (typeof data === "string") {
-        try {
-            data = JSON.parse(data);
-        } catch (error) {
-            console.error("    JSON parse error:", error)
-        }
+    let data;
+    try {
+        data = JSON.parse(dataStr);
+    } catch (error) {
+        console.error("    failed to JSON parse the POST data")
+        response.status(500).send("Internal server error");
+        return;
     }
 
     // Return the response early, don't wait for the background processing
     response.status(200).send("ok");
 
-    // noinspection JSIgnoredPromiseFromCall
+    // noinspection ES6MissingAwait
     compileByEntryId(data?.object?.id, request, response);
 }
